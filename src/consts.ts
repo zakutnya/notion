809a2d4a2a2721118a9247ec758f009c428a2b35
zakutnya  <yazakutny@gmail.com>
2024-05-26 21:24:10 +0300

    added posts listing, fixed links, added About page

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-    NAME: "Astro Nano",
+    NAME: "Exil Solidaire",
     EMAIL: "markhorn.dev@gmail.com",
     NUM_POSTS_ON_HOMEPAGE: 3,
     NUM_WORKS_ON_HOMEPAGE: 2,
