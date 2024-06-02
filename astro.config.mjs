033ad945c52210690a2a78fbf5f0ebb2ee0bb2f0
zakutnya  <yazakutny@gmail.com>
2024-06-02 11:43:31 +0300

    more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         port: 3000
     },
     site: 'https://zakutnya.github.io',
-    base: '',
+    base: '/notion',
     trailingSlash: "ignore",
     integrations: [mdx(), tailwind(), pagefind()],
     build: {
